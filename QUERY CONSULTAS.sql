e3233b5e6e4a0258b85c51a6be01ae7bd0a5f550
Nicolas <duquenicolas391@gmail.com>
2023-09-12 08:27:52 -0500

    Actualizadas algunas querys

--- QUERY CONSULTAS.sql
+++ QUERY CONSULTAS.sql
@@ -6,7 +6,7 @@ SELECT id, ciudad FROM oficina;
 
 SELECT ciudad, telefono FROM oficina WHERE pais = 'Indonesia';
 
--- 3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo
+-- 3. Devuelve un listado con el nombre, apellidos e email de los empleados cuyo
 -- jefe tiene un código de jefe igual a 7.
  
 SELECT nombre, apellido1, apellido2, email FROM empleado WHERE id_jefe = 7;
@@ -18,9 +18,9 @@ SELECT nombre, apellido1, apellido2, email FROM empleado WHERE id_jefe = 7;
 -- • Utilizando la función DATE_FORMAT de MySQL
 
 SELECT DISTINCT cliente_codigo FROM pagos WHERE YEAR(fecha_pago) = 2008;
-
--- SELECT DISTINCT cliente_codigo FROM pagos DATE_FORMAT('fecha_pago', '%Y')
-
+SELECT cliente_codigo, DATE_FORMAT(p.fecha_pago, '%Y') AS año FROM pagos p 
+ WHERE YEAR(fecha_pago) = 2008;
+ 
 -- 5. ¿Cuántos empleados hay en la compañía?
 
 SELECT COUNT(*) as cantidad_empleados FROM empleado;
@@ -33,7 +33,7 @@ GROUP BY pais;
 
 -- 7. ¿Cuál fue el pago medio en 2007?
 
-SELECT AVG(total_pago) FROM pagos 
+SELECT ROUND(AVG(total_pago)) as pago_medio_2007 FROM pagos 
 WHERE YEAR(fecha_pago) = 2007;
 
 -- 8. ¿Cuántos pedidos hay en cada estado? Ordena el resultado de forma
@@ -78,8 +78,8 @@ LIMIT 1;
 
 SELECT nombre_cliente
 FROM cliente
-WHERE limite_credito 
-> (SELECT SUM(total_pago) FROM pagos WHERE pagos.cliente_codigo = cliente.id);
+WHERE limite_credito > 
+(SELECT SUM(total_pago) FROM pagos WHERE pagos.cliente_codigo = cliente.id);
 
 -- 14. Devuelve el listado de clientes indicando el nombre del cliente y cuantos
 -- pedidos ha realizado. Tenga en cuenta que pueden existir clientes que no
@@ -87,20 +87,16 @@ WHERE limite_credito
 
 SELECT c.nombre_cliente, COUNT(p.id) AS cantidad_de_pedidos
 FROM cliente c
-LEFT JOIN pedidos p ON c.id = p.id_Cliente
+JOIN pedidos p ON c.id = p.id_Cliente
 GROUP BY c.nombre_cliente;
 
 -- 15. Devuelve el nombre, apellidos, puesto y teléfono de la oficina de aquellos
 -- empleados que no sean representante de ventas de ningún cliente.
 
-SELECT 
-  e.nombre, e.apellido1, e.apellido2, e.puesto, o.telefono
-FROM 
-  empleado e
-JOIN 
-  oficina o ON e.id_oficina = o.id
-WHERE 
-  e.id NOT IN (SELECT id_empleado FROM cliente WHERE id_empleado IS NOT NULL);
+SELECT e.nombre, e.apellido1, e.apellido2, e.puesto, o.telefono
+FROM empleado e
+JOIN oficina o ON e.id_oficina = o.id
+WHERE e.id NOT IN (SELECT id_empleado FROM cliente WHERE id_empleado IS NOT NULL);
 
 -- 16. Devuelve las oficinas donde no trabajan ninguno de los empleados que
 -- hayan sido los representantes de ventas de algún cliente que haya realizado
@@ -124,7 +120,7 @@ WHERE o.id NOT IN (
 
 SELECT c.nombre_cliente, COUNT(p.id) AS cantidad_de_pedidos
 FROM cliente c
-LEFT JOIN pedidos p ON c.id = p.id_cliente
+JOIN pedidos p ON c.id = p.id_cliente
 GROUP BY c.nombre_cliente;
 
 -- 18. Devuelve un listado con los nombres de los clientes y el total pagado por
@@ -133,5 +129,5 @@ GROUP BY c.nombre_cliente;
 
 SELECT c.nombre_cliente, SUM(p.total_pago) AS total_pagado
 FROM cliente c
-LEFT JOIN pagos p ON c.id = p.cliente_codigo
+JOIN pagos p ON c.id = p.cliente_codigo
 GROUP BY c.nombre_cliente;
